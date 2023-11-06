f4546c3e3efb86986dae6de5bc1b5ce4cae78949
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-11-06 18:56:27 +0100

    handleClick in player.js done!

--- spotify-javier/src/components/Player.jsx
+++ spotify-javier/src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const Pause = ({ className }) => (
     <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -10,6 +10,21 @@ export const Pause = ({ className }) => (
 
   export function Player () {
     const [isPlaying, setIsPlaying] = useState(false)
+    const [currentSong, setCurrentSon] = useState(null)
+    const audioRef = useRef()
+
+    const handleClick = () => {
+        if (isPlaying) {
+            audioRef.current.pause()
+        } else {
+            audioRef.current.src = `/music/1/01.mp3`
+            audioRef.current.play()
+            audioRef.current.volume = 0.2
+        }
+        
+        setIsPlaying(!isPlaying)
+    }
+    
     return (
         <div className="flex flex-row justify-between w-full px-4 z-50">
             <div>
@@ -18,15 +33,17 @@ export const Pause = ({ className }) => (
 
             <div className="grid place-content-center gap-4 flex-1">
                 <div className="flex justify-center">
-                    <button className="bg-white rounded-full p-2" onClick={() => setIsPlaying(!isPlaying)}>
+                    <button className="bg-white rounded-full p-2" onClick={handleClick}>
                     {isPlaying ? <Pause /> : <Play />}
                     </button>
                 </div>
             </div>
 
-            <div>
+            <div className="grid place-content-center">
                 Volumen...
             </div>
+
+            <audio ref={audioRef} />
         </div>
     )
   }
