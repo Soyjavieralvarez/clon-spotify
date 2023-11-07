e5b5b37b7d05956701d43e4de9e09afaefbe09e4
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-11-07 19:16:55 +0100

    playerstore.js created

--- spotify-javier/src/components/CardPlayButton.jsx
+++ spotify-javier/src/components/CardPlayButton.jsx
@@ -2,7 +2,7 @@ import { Pause, Play } from "./Player"
 
 export function CardPlayButton({id}) {
     return (
-        <div className="card-play-button">
+        <div className="card-play-button rounded-full bg-green-500 p-5">
             <Play />
         </div>
     )
