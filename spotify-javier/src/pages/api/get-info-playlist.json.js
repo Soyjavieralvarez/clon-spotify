8dfe6eb6f46865efb7f83c4c17b48a0a919b4d4f
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-11-08 18:07:58 +0100

    get-info imported

--- spotify-javier/src/pages/api/get-info-playlist.json.js
+++ spotify-javier/src/pages/api/get-info-playlist.json.js
@@ -1,3 +1,7 @@
+import { allPlaylists, songs } from "@/lib/data";
+
 export async function GET({ params, request}) {
-    
+    // get the id from the url search params
+
+    const { url } = request
 }
